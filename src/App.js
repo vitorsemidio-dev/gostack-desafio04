becc279ef7295014906d6ef2ea67e526a1652b7c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-09 00:55:38 -0300

    Importação de imagem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import snk from '../src/assets/snk01.jpg'
 
 function App() {
-  return <h1>SNK - Shingeki no Kyojin</h1>
+  return (
+    <>
+    <h1>SNK - Shingeki no Kyojin</h1>
+    <img width="1024" src={snk} alt="Shingeki no Kyojin"/>
+    </>
+  )
 }
 
 export default App;
