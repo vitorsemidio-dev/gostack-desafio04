7a5e06b3a07699ec70022216b0bda948732fd0e0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-09 14:21:14 -0300

    Criação componentes

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Comment extends Component {
+  render() {
+    return <div />;
+  }
+}
---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class PostList extends Component {
+  render() {
+    return <div />;
+  }
+}
