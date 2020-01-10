c2101e55ddac670e746dcca179f3815c461e3eee
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-10 00:26:45 -0300

    Alinhamento post list

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -3,25 +3,19 @@ import React, { Component } from 'react';
 import Post from '../Post/Post';
 import { Posts } from '../../db/PostsMock';
 
-// import { Container } from './styles';
+import './PostList.css';
 
 export default class PostList extends Component {
   state = {
     posts: Posts,
   };
 
-  componentDidMount() {
-    // console.log(this.state.posts);
-    // console.log('Conteudo do post:');
-    // this.state.posts.forEach(p => console.log(p.content));
-  }
   render() {
     return (
       <>
-        <h1>PostList</h1>
-        <ul>
+        <div class="post-list">
           {this.state.posts.map(p => <Post key={p.id} data={p} />)}
-        </ul>
+        </div>
       </>
     );
   }
